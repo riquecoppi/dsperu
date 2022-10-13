f9a24bd15dc28ebde1557a28b828b4c6db67312d
Henrique C. Ribeiro <riquecoppi@gmail.com>
2022-10-13 20:34:10 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/sistemacoppi/dsperu/DsperuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemacoppi.dsperu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsperuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
